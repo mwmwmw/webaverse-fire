bdc16f3830e93dcf999cebe1f4b580b1956d6133
Matthew Willox <matthew@cybermancers.ca>
2022-05-08 10:50:42 -0600

    textures

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import metaversefile from 'metaversefile';
 import Fire from './fire';
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
-const {useApp, useFrame } = metaversefile;
+const { useApp, useFrame } = metaversefile;
 
 export default () => {
   const app = useApp();
